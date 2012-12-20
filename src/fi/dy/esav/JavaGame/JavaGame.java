765812d2d8b3495ba6a63466c54bf189e93a5756
Esa Varemo <esa@kuivanto.fi>
2012-12-20 16:46:28 +0200

    Shut down the engine when the window closes

--- src/fi/dy/esav/JavaGame/JavaGame.java
+++ src/fi/dy/esav/JavaGame/JavaGame.java
@@ -9,6 +9,22 @@ public class JavaGame {
 	public static void main(String[] args) {
 		GameEngine engine = new GameEngine();
 		engine.getStage().setVisible(true);
+
+		while(!engine.getStage().isValid()) continue;
+		
+		while(engine.getStage().isVisible()) {
+			//System.out.println(engine.getInputstate().getMouseX() + ", " + engine.getInputstate().getMouseY());
+			//System.out.println(engine.getInputstate().getKeyboardState().toString());
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("Closing");
+		engine.getStage().dispose();
 	}
 
 }
