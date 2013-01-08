499aea26e7d98f931ff2e775693e4a7161e81422
Esa Varemo <esa@kuivanto.fi>
2013-01-08 22:26:29 +0200

    Created the "world"

--- src/fi/dy/esav/JavaGame/World.java
+++ src/fi/dy/esav/JavaGame/World.java
@@ -9,12 +9,23 @@ public class World {
 		int stageWidth = JavaGame.getEngine().getStage().getContentPane().getWidth();
 		int stageHeight = JavaGame.getEngine().getStage().getContentPane().getHeight();
 		
-		Rectangle[] ground = {  new Rectangle(0, 0, 10, stageHeight), 
-								new Rectangle(0, 0, stageWidth, 10),
-								new Rectangle(0, stageHeight-10, stageWidth, 10),
-								new Rectangle(stageWidth-10, 0, 10, stageHeight),
+		int storyHeight = stageHeight/4;
+		int widthUnit = stageWidth/16;
+		
+		Rectangle[] ground = {  new Rectangle(0, 				0, 				10, 		stageHeight), 
+								new Rectangle(0, 				0, 				stageWidth, 10),
+								new Rectangle(0, 				stageHeight-10, stageWidth, 10),
+								new Rectangle(stageWidth-10, 	0, 				10, 		stageHeight),
 								
+								new Rectangle(3*widthUnit,				storyHeight,	4*widthUnit,	10),
+								new Rectangle(stageWidth-7*widthUnit,	storyHeight,	4*widthUnit,	10),
 								
+								new Rectangle(0, 						storyHeight*2, 4*widthUnit, 10),
+								new Rectangle(6*widthUnit, 				storyHeight*2, 4*widthUnit, 10),
+								new Rectangle(stageWidth-4*widthUnit, 	storyHeight*2, 4*widthUnit, 10),
+								
+								new Rectangle(3*widthUnit,				storyHeight*3,	4*widthUnit,	10),
+								new Rectangle(stageWidth-7*widthUnit,	storyHeight*3,	4*widthUnit,	10),
 								};
 		
 		for (Rectangle rect : ground) {
