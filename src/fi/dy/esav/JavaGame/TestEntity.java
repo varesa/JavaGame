f1e5d5fc0b73a1d2364838ed1c307c1d52657d01
Esa Varemo <esa@kuivanto.fi>
2013-01-08 14:49:18 +0200

    Fixed the image path, some moving testing...

--- src/fi/dy/esav/JavaGame/TestEntity.java
+++ src/fi/dy/esav/JavaGame/TestEntity.java
@@ -11,15 +11,26 @@ public class TestEntity extends Entity {
 	
 	Image sprite;
 	
+	private double x = 0;
+	private double y = 500;
+	
 	public TestEntity(GameEngine engine) {
 		super(engine);
 		
-		sprite = Toolkit.getDefaultToolkit().getImage("../res/test.png");
+		sprite = Toolkit.getDefaultToolkit().getImage("res/test.png");
+		System.out.println(sprite.getHeight(engine.getStage()));
 	}
 	
 	@Override
 	public void draw(Graphics g) {
-		System.out.println("drawing");
-		g.drawImage(sprite, 50, 50, engine.getStage());
+		//System.out.println("drawing");
+		//System.out.println(sprite.getHeight(engine.getStage()));
+		g.drawImage(sprite, (int) x, (int) y, sprite.getWidth(engine.getStage()), sprite.getHeight(engine.getStage()), engine.getStage());
+	}
+	
+	@Override
+	public void act() {
+		this.x += 0.5;
+		this.y -= 0.9;
 	}
 }
