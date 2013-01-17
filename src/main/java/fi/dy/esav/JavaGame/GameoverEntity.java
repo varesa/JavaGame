51fda7d8ede5e9854d8bc71d0100b31691c39221
Esa Varemo <esa@kuivanto.fi>
2013-01-17 22:58:25 +0200

    Show the score

--- src/main/java/fi/dy/esav/JavaGame/GameoverEntity.java
+++ src/main/java/fi/dy/esav/JavaGame/GameoverEntity.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import fi.dy.esav.GameEngine.Entity;
 import fi.dy.esav.GameEngine.GameEngine;
@@ -27,6 +29,17 @@ public class GameoverEntity extends Entity {
 		FontMetrics fm = g.getFontMetrics(font);
 		g.drawString(maintext, engine.getStage().getContentPane().getWidth()
 				/ 2 - fm.stringWidth(maintext) / 2, 200);
+		
+		SimpleDateFormat format = new SimpleDateFormat("mm:ss");
+		String time = format.format(new Date(JavaGame.getWorld().getScore().getTime()));
+		
+		String score = "Your score was: " + JavaGame.getWorld().getScore().getKills() + " in " + time; 
+		Font font3 = new Font("Arial", Font.ITALIC, 35);
+		g.setFont(font3);
+		g.setColor(Color.RED);
+		FontMetrics fm3 = g.getFontMetrics(font3);
+		g.drawString(score, engine.getStage().getContentPane().getWidth()
+				/ 2 - fm3.stringWidth(score) / 2, 300);
 		
 		Font font2 = new Font("Arial", Font.ITALIC, 35);
 		g.setFont(font2);
