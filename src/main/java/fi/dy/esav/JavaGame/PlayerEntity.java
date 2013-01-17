b555daaaf63c156c220ccb6cd64761694fa89e2c
Esa Varemo <esa@kuivanto.fi>
2013-01-17 16:49:42 +0200

    A bit slower rate of fire ASSIGNED - # 27: TODO: Implement shooting http://bt.esav.dy.fi/view.php?id=27

--- src/main/java/fi/dy/esav/JavaGame/PlayerEntity.java
+++ src/main/java/fi/dy/esav/JavaGame/PlayerEntity.java
@@ -8,47 +8,57 @@ import fi.dy.esav.GameEngine.GameEngine;
 import fi.dy.esav.JavaGame.enums.DIRECTION;
 
 public class PlayerEntity extends LivingEntity {
-	
+
 	private Color color = Color.BLUE;
+
+	private long canShoot;
+	private long shootDelay = 500;
 
 	public PlayerEntity(GameEngine engine) {
 		super(engine);
-		
+
 		height = 48;
 		width = 48;
+
+		canShoot = System.currentTimeMillis();
 	}
-	
+
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(color);
-		g.drawRect((int)x, (int)y, width, height);
+		g.drawRect((int) x, (int) y, width, height);
 	}
-	
+
 	@Override
 	public void act() {
-		if(this.engine.getInputState().isKeyDown(KeyEvent.VK_LEFT)) {
+		if (this.engine.getInputState().isKeyDown(KeyEvent.VK_LEFT)) {
 			this.xtarget = -1;
-		} else if (this.engine.getInputState().isKeyDown(KeyEvent.VK_RIGHT)){
+		} else if (this.engine.getInputState().isKeyDown(KeyEvent.VK_RIGHT)) {
 			this.xtarget = +1;
 		} else {
 			this.xtarget = 0;
 		}
-		
-		if(this.engine.getInputState().isKeyDown(KeyEvent.VK_UP)) {
+
+		if (this.engine.getInputState().isKeyDown(KeyEvent.VK_UP)) {
 			this.jump = true;
 		} else {
 			this.jump = false;
 		}
-		
-		if(this.engine.getInputState().isKeyDown(KeyEvent.VK_SPACE)) {
-			if(facing == DIRECTION.LEFT) {
-				this.engine.addEntity(new Bullet(this.getPos(), DIRECTION.LEFT, this.engine));
-			} else if(facing == DIRECTION.RIGHT) {
 
-				this.engine.addEntity(new Bullet(this.getPos(), DIRECTION.RIGHT, this.engine));
+		if (this.engine.getInputState().isKeyDown(KeyEvent.VK_SPACE)) {
+			if (canShoot <= System.currentTimeMillis()) {
+				if (facing == DIRECTION.LEFT) {
+					this.engine.addEntity(new Bullet(this.getPos(),
+							DIRECTION.LEFT, this.engine));
+				} else if (facing == DIRECTION.RIGHT) {
+
+					this.engine.addEntity(new Bullet(this.getPos(),
+							DIRECTION.RIGHT, this.engine));
+				}
+				canShoot = System.currentTimeMillis() + shootDelay;
 			}
 		}
-		
+
 		super.act();
 
 	}
