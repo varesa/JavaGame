f238ad0f744046cd81b0463205a6b95f4ed23d3a
Esa Varemo <esa@kuivanto.fi>
2013-01-17 22:42:12 +0200

    Added timekeeping + ability to read the score

--- src/main/java/fi/dy/esav/JavaGame/Score.java
+++ src/main/java/fi/dy/esav/JavaGame/Score.java
@@ -1,10 +1,34 @@
 package fi.dy.esav.JavaGame;
 
 public class Score {
-	int kills = 0;
-	long times = 0;
+	private int kills = 0;
+	
+	private long startTime;
+	private boolean running = true;
+	private long stopTime;
+	
+	public Score() {
+		startTime = System.currentTimeMillis();
+	}
 	
 	public void increase() {
 		kills++;
+	}
+	
+	public int getKills() {
+		return kills;
+	}
+	
+	public void stopCounting() {
+		stopTime = System.currentTimeMillis();
+		running = false;
+	}
+	
+	public long getTime() {
+		if(running) {
+			return System.currentTimeMillis() - startTime;
+		} else {
+			return stopTime - startTime;
+		}
 	}
 }
