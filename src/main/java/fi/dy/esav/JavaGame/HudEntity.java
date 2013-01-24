c938ede17cd028675614582341f2c4168c3868e3
Esa Varemo <esa@kuivanto.fi>
2013-01-24 11:48:03 +0200

    Commented HudEntity class and methods Disabled default constructor

--- src/main/java/fi/dy/esav/JavaGame/HudEntity.java
+++ src/main/java/fi/dy/esav/JavaGame/HudEntity.java
@@ -7,13 +7,31 @@ import fi.dy.esav.GameEngine.Entity;
 import fi.dy.esav.GameEngine.GameEngine;
 import fi.dy.esav.GameEngine.enums.ENTITY;
 
+/**
+ * A HUD to display information ingame
+ */
+
 public class HudEntity extends Entity {
 
+	/**
+	 * Disabled parameterless constructor
+	 */
+	private HudEntity() { super(null);	}
+	
+	/**
+	 * Default constructor
+	 * @param engine reference to the engine
+	 */
 	public HudEntity(GameEngine engine) {
 		super(engine);
 		this.setProperty(ENTITY.NO_ACT);
 	}
 	
+	/**
+	 * Method to do the graphics drawing in
+	 * overrides parent class template method
+	 * @param g Graphics instance to draw on
+	 */
 	@Override
 	public void draw(Graphics g) {
 		String score = String.valueOf(JavaGame.getWorld().getScore().getKills());
@@ -21,7 +39,4 @@ public class HudEntity extends Entity {
 		g.setFont(font);
 		g.drawString(score, 20, engine.getStage().getContentPane().getHeight()-20);
 	}
-	
-	
-
 }
