2cf1d2b7de356aa4075b3cbba3011d3948c18fa1
Esa Varemo <esa@kuivanto.fi>
2013-01-24 11:51:43 +0200

    Commented the GroundEntity class and methods Disabled the default constructors

--- src/main/java/fi/dy/esav/JavaGame/GroundEntity.java
+++ src/main/java/fi/dy/esav/JavaGame/GroundEntity.java
@@ -8,15 +8,33 @@ import fi.dy.esav.GameEngine.Entity;
 import fi.dy.esav.GameEngine.GameEngine;
 import fi.dy.esav.GameEngine.enums.ENTITY;
 
+/**
+ * A entity to build the stage ground from
+ */
+
 public class GroundEntity extends Entity {
 	
 	private int width = 10, height = 10;
 	private Color color = Color.BLACK;
 
+	/**
+	 * Disabled parameterless constructor
+	 */
+	private GroundEntity() { super(null);	}
+	
+	/**
+	 * Default constructor
+	 * @param engine reference to the GameEngine
+	 */
 	public GroundEntity(GameEngine engine) {
 		super(engine);
 	}
 	
+	/**
+	 * A constructor with the ground box defined
+	 * @param engine reference to the GameEngine
+	 * @param rect the rectangle to become the part of ground
+	 */
 	public GroundEntity(GameEngine engine, Rectangle rect) {
 		super(engine);
 		
@@ -28,6 +46,11 @@ public class GroundEntity extends Entity {
 		height = rect.height;
 	}
 	
+	/**
+	 * Method to do the drawing in
+	 * overrides parent class template method
+	 * @param g Graphics to draw on
+	 */
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(color);
@@ -35,6 +58,8 @@ public class GroundEntity extends Entity {
 	}
 
 	/**
+	 * Get the width of the entity
+	 * overrides parent class template method
 	 * @return the width
 	 */
 	@Override
@@ -43,6 +68,8 @@ public class GroundEntity extends Entity {
 	}
 
 	/**
+	 * Set the width of the entity
+	 * overrides parent class template method
 	 * @param width the width to set
 	 */
 	public void setWidth(int width) {
@@ -50,6 +77,8 @@ public class GroundEntity extends Entity {
 	}
 
 	/**
+	 * get the entity height
+	 * overrides parent class template method
 	 * @return the height
 	 */
 	@Override
@@ -58,6 +87,8 @@ public class GroundEntity extends Entity {
 	}
 
 	/**
+	 * set the entity height
+	 * overrides parent class template method
 	 * @param height the height to set
 	 */
 	public void setHeight(int height) {
