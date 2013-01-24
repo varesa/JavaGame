0126b1dcd673b8d89363f2b70afa0d491cf9b4bc
Esa Varemo <esa@kuivanto.fi>
2013-01-24 11:28:14 +0200

    Commenting Score class methods and class

--- src/main/java/fi/dy/esav/JavaGame/Score.java
+++ src/main/java/fi/dy/esav/JavaGame/Score.java
@@ -1,5 +1,9 @@
 package fi.dy.esav.JavaGame;
 
+
+/**
+ * Class to keep score and spent time in
+ */
 public class Score {
 	private int kills = 0;
 	
@@ -7,23 +11,40 @@ public class Score {
 	private boolean running = true;
 	private long stopTime;
 	
+	/**
+	 * Default constructor
+	 */
 	public Score() {
 		startTime = System.currentTimeMillis();
 	}
 	
+	/**
+	 * Increase the kills
+	 */
 	public void increase() {
 		kills++;
 	}
 	
+	/**
+	 * Get the amount of kills
+	 * @return amount of kills
+	 */
 	public int getKills() {
 		return kills;
 	}
 	
+	/**
+	 * Stop counting time
+	 */
 	public void stopCounting() {
 		stopTime = System.currentTimeMillis();
 		running = false;
 	}
 	
+	/**
+	 * Get current time
+	 * @return current time
+	 */
 	public long getTime() {
 		if(running) {
 			return System.currentTimeMillis() - startTime;
