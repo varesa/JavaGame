5725bd42f2c2cd28f460090364c216561aae485f
Esa Varemo <esa@kuivanto.fi>
2013-01-24 11:43:22 +0200

    Commented JumpAiNode class and methods

--- src/main/java/fi/dy/esav/JavaGame/JumpAiNode.java
+++ src/main/java/fi/dy/esav/JavaGame/JumpAiNode.java
@@ -16,10 +16,22 @@ public class JumpAiNode extends AiNode {
 	 */
 	private JumpAiNode() { super(null);	}
 	
+	/**
+	 * The default constructor
+	 * @param engine reference to the GameEngine
+	 */
 	public JumpAiNode(GameEngine engine) {
 		super(engine);
 	}
 
+	
+	/**
+	 * Constructor with the location and direction defined
+	 * @param engine reference to the GameEngine
+	 * @param x the x-coordinate of the node to be created
+	 * @param y the y-coordinate of the node to be created
+	 * @param direction direction of the node to be created
+	 */
 	public JumpAiNode(GameEngine engine, int x, int y, AINODE direction) {
 		super(engine, x, y);
 		
@@ -29,10 +41,18 @@ public class JumpAiNode extends AiNode {
 		this.direction = direction;
 	}
 	
+	/**
+	 * Get the direction of the node
+	 * @return the direction
+	 */
 	public AINODE getDirection() {
 		return direction;
 	}
 	
+	/**
+	 * Set the direction of the node
+	 * @param direction new direction
+	 */
 	public void setDirection(AINODE direction) {
 		this.direction = direction;
 	}
