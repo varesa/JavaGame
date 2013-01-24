261dce1de78444166f69c37eea5bebfeacbd3f85
Esa Varemo <esa@kuivanto.fi>
2013-01-24 11:31:56 +0200

    Commenting MouseTrackerEntity class methods and class

--- src/main/java/fi/dy/esav/JavaGame/MouseTrackerEntity.java
+++ src/main/java/fi/dy/esav/JavaGame/MouseTrackerEntity.java
@@ -5,10 +5,15 @@ import fi.dy.esav.GameEngine.GameEngine;
 
 /**
  * This is a tool for debugging / map creation
+ * It prints out the mouse position to the console every ten frames
  */
 
 public class MouseTrackerEntity extends Entity {
 
+	/**
+	 * Default constructor
+	 * @param engine reference to the GameEngine
+	 */
 	public MouseTrackerEntity(GameEngine engine) {
 		super(engine);
 		// TODO Auto-generated constructor stub
@@ -16,6 +21,10 @@ public class MouseTrackerEntity extends Entity {
 	
 	int a = 0;
 	
+	/**
+	 * Method to do the logic
+	 * Overrides parent class template method
+	 */
 	@Override
 	public void act() {
 		if(a == 10) { 
